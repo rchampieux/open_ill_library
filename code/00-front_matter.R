5317dbdb719d629d0aae32bad7eae72feac57b55
Jessica Minnier <jessica.minnier@gmail.com>
2018-06-19 13:34:05 -0700

    pull out front matter

---
+++ code/00-front_matter.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(readxl)
+library(skimr)
+library(janitor)
+library(here)
+
+#info from http://tophcito.blogspot.com/2015/11/accessing-apis-from-r-and-little-r.html
+library(httr)
+library(jsonlite)
+library(printr)
+
+date_updated <- "2018-06-18"
+unpaywall_datafile <- paste0(date_updated,"-unpaywall_raw.RData")
+unpaywall_results_file <- paste0(date_updated,"-unpaywall_results.csv")
+#unpaywall_results_file <- paste0(lubridate::today(),"_unpaywall.csv")
+update_raw_data <- FALSE
+
+knitr::opts_chunk$set(
+  eval       = TRUE,    # whether to run code in code chunk
+  include    = TRUE,    # whether to include the chunk output
+  echo       = TRUE,   # Whether to show code chunk in final output
+  error      = TRUE,    # whether to display error messages
+  message    = FALSE,   # whether to preserve messages
+  warning    = FALSE,   # whether to preserve warnings
+  comment    = "#>",    # a character string to append at start
+  # of each line of results in final document
+  tidy       = FALSE,   # whether to tidy code chunks for display
+  dpi        = 96, 
+  fig.width  = 6,       # consistent width for figures
+  fig.asp    = 0.618,   # the golden ratio, can be adjusted in individual chunks
+  out.width  = "100%",   # controls the output size
+  fig.align  = "center" # give plot room to breathe
+)
---
+++ code/01-pull_oabutton_data.R
@@ -0,0 +1,13 @@
+#'---
+#'title: "Pull OA Button Data"
+#'author: "Jessica Minnier"
+#'date: '`r Sys.Date()`'
+#'output: github_document
+#'---
+
+#+ r setup, include=FALSE
+source(here("code","00-front_matter.R"))
+
+#' # Read Data
+
+source(here("code","00-clean_input_data.R"))
